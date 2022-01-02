e35c40b8453f6c435908803a940a9640d0ee79dd
ankita21gaikwad <96906679+ankita21gaikwad@users.noreply.github.com>
2022-01-02 22:05:05 +0530

    Create generate-random-number.ts

---
+++ generate-random-number.ts
@@ -0,0 +1,9 @@
+// 6. Generate random number 
+
+function generateRandomNumber(): number{
+    let randomNumber: number = Math.random();
+    console.log('Generated Random Number is ' +randomNumber);
+    return randomNumber;
+}
+
+generateRandomNumber();
