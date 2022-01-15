b3f8e98b692b8d27aabb4d8797682669122f2f34
ankita21gaikwad <96906679+ankita21gaikwad@users.noreply.github.com>
2022-01-15 15:09:20 +0530

    Update fibonacci-series.ts

--- fibonacci-series.ts
+++ fibonacci-series.ts
@@ -1,5 +1,6 @@
 // 1.Fibonacci Series : 0,1,1,2,3,5,8,13,21........
 
+function findFibonacciSeries(){
 let n1 = 0;
 let n2 = 1;
 console.log(n1);
@@ -10,3 +11,7 @@ for(let i =1; i<8; i++){
     n1 = n2; 
     n2 = n3;    
 }
+}
+
+
+findFibonacciSeries();
