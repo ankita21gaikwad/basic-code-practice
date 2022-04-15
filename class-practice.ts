f2294bdc6e8a28f64e596c087ec3470eea6c90dd
ankita21gaikwad <96906679+ankita21gaikwad@users.noreply.github.com>
2022-04-15 14:56:49 +0530

    add class inheritance

--- class-practice.ts
+++ class-practice.ts
@@ -1,4 +1,4 @@
-// Creating a class
+// ***********Creating a Class***********
 // Class : A class in terms of OOP is a blueprint for creating objects. A class encapsulates data for the object
 class Car {
     // 1. field : A field is any variable declared in a class. Fields represent data pertaining to objects
@@ -24,3 +24,29 @@ console.log('Reading attribute value Model as : ' + obj.model);
 
 // Access the method/function
 obj.displayModelName();
+
+// ***************************************************************************************************************************** //
+
+
+// ***********Class Inheritance***********
+// Inheritance is the ability to create newclasses from an existing class 
+// The class that is extended to create newer classes is called the parent class/super class. 
+// The newly created classes are called the child/sub classes.
+// A class inherits from another class using the ‘extends’ keyword. 
+// Child classes inherit all properties and methods except private members and constructors from the parent class.
+class AutoMobile {
+    Price: string;
+
+    constructor(price: string) {
+        this.Price = price;
+    }
+}
+
+class Audi extends AutoMobile{
+    displayPrice(): void {
+        console.log('Price of AUDI is :' + this.Price);
+    }
+}
+
+var audi = new Audi('$41,800');
+audi.displayPrice();
