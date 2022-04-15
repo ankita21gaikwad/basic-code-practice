ec229b0766f67adccda2675bbb13ebb268d09ee7
ankita21gaikwad <96906679+ankita21gaikwad@users.noreply.github.com>
2022-04-15 13:12:59 +0530

    commit Class basics

--- class-practice.js
+++ class-practice.js
@@ -1,26 +1,19 @@
 // Creating a class
 // Class : A class in terms of OOP is a blueprint for creating objects. A class encapsulates data for the object
-class Car {
-    // 1. field : A field is any variable declared in a class. Fields represent data pertaining to objects
-    model: string;
-
+var Car = /** @class */ (function () {
     // 2. constructor : Responsible for allocating memory for the objects of the class.
-    constructor(model: string) {
+    function Car(model) {
         this.model = model;
     }
-
     // 3. method/function : Functions represent actions an object can take. They are also at times referred to as methods
-    displayModelName(): void {
+    Car.prototype.displayModelName = function () {
         console.log('Model of the Car is : ' + this.model);
-    }
-
-}
-
+    };
+    return Car;
+}());
 // Create an object of a class
 var obj = new Car('AUDI');
-
 // Access the field
 console.log('Reading attribute value Model as : ' + obj.model);
-
 // Access the method/function
 obj.displayModelName();
