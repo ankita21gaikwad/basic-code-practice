1179c93b739fb38b9fe1f019111d2cf66d59e66c
ankita21gaikwad <ankita21gaikwad@gmail.com>
2024-03-04 18:15:27 +0530

    add `problem1.ts` in array-problems

---
+++ src/array-problems/problem1.ts
@@ -0,0 +1,16 @@
+// Question 1: find the index of the elements of the array whose sum is 15
+const array = [2, 11, 8, 9, 13];
+let index1 = 0;
+let index2 = 0;
+
+for(let i = 0; i < array.length; i++) {
+  for (let j = i+1; j < array.length; j++) {
+    if (array[i] + array[j] === 15) {
+      index1 = i;
+      index2 = j;
+    }
+  }
+}
+
+console.log('index 1 is ' + index1);
+console.log('index 2 is ' + index2);
