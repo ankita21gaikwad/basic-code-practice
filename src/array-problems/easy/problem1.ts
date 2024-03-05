df9dca5f1f27a22beac1edebd4bbfbccefbb7d82
ankita21gaikwad <ankita21gaikwad@gmail.com>
2024-03-05 22:28:01 +0530

    add `problem1.ts` in `array-problems/east`

--- src/array-problems/easy/problem1.ts
+++ src/array-problems/easy/problem1.ts
@@ -2,19 +2,44 @@
 // Input: arr[] = {10, 4, 3, 50, 23, 90}
 // Output: 90, 50, 23
 
-let arr = [10, 4, 3, 50, 23, 90];
+let arrProblem1 = [10, 4, 3, 50, 23, 90];
+function find3Largest(array: number[]) {
+  // find length of the array
+  let arrayLength = array.length;
 
-for (let i = 0; i < arr.length; i++) {
-   for (let j = i + 1; j < arr.length; j++) {
-     if (arr[j] > arr[i]) {
-       let temp = arr[i];
-       arr[i] = arr[j];
-       arr[j] = temp;
-     }
-   }
+  // throw error if length of array is less than 3
+  if (arrayLength < 3) {
+    throw new Error('Array is invalid! We are expecting at least 3 elements in the array.')
+  }
+
+  // initialize largest 3 numbers to minimum value
+  let first = Number.MIN_VALUE;
+  let second = Number.MIN_VALUE;
+  let third = Number.MIN_VALUE;
+
+  // iterate through the elements
+  for (let i = 0; i < arrayLength; i++) {
+    if (array[i] > first) {
+      third = second;
+      second = first;
+      first = array[i];
+    }
+
+    else if (array[i] > second) {
+      third = second;
+      second = array[i];
+    }
+
+    else if (array[i] > third) {
+      third = array[i];
+    }
+  }
+
+  return `${first}, ${second}, ${third}`;
 }
 
-console.log(`${arr[0]}, ${arr[1]}, ${arr[2]}`);
+console.log(find3Largest(arrProblem1));
+
 
 
 
